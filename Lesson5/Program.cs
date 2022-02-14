7a4843f4cd0a6225cac7d9bf58ba9fa6b618fe43
RiiFus <di.22rus@yandex.ru>
2022-02-14 19:39:34 +0700

    Задание1

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lesson5
 {
@@ -10,6 +11,10 @@ namespace Lesson5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите данные для сохранения в файл:");
+            string text = Console.ReadLine();
+            string filename = "text.txt";
+            File.WriteAllText(filename, text);
         }
     }
 }
