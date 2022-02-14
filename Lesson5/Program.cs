d588132b18ba0ba5f33e73d99a4086a38ab47898
RiiFus <di.22rus@yandex.ru>
2022-02-14 19:45:48 +0700

    Задание2

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Lesson5
 {
     class Program
     {
         static void Main(string[] args)
         {
+            File.AppendAllText("startup.txt", DateTime.Now.ToShortTimeString());
+            File.AppendAllText("startup.txt", Environment.NewLine);
         }
     }
 }
